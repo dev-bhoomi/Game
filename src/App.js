b28b8595ccbfaeeff18f1efdb9d21ced215ffc4d
dev-bhoomi <bhoomigupta912536@gmail.com>
2023-10-25 12:05:18 +0530

    react

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import { useEffect, useState } from 'react'
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/Game/img/helmet-1.png", matched: false },
-  { "src": "/Game/img/potion-1.png", matched: false },
-  { "src": "/Game/img/ring-1.png", matched: false },
-  { "src": "/Game/img/scroll-1.png", matched: false },
-  { "src": "/Game/img/shield-1.png", matched: false },
-  { "src": "/Game/img/sword-1.png", matched: false }
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false }
 ];
 
 const App = () => {
